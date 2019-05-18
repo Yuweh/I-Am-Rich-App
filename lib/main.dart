0b6061ba6d5fa7b238a4f53f4bdb7ec084bed561
yuweh <francisb.itcareers@gmail.com>
2019-05-18 23:04:30 +0800

    app finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+//the main function is the starting point for all our Flutter apps.
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ), //Center
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ), //AppBar
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ), //Image
+        ), //Center
+      ), //Scaffold
     ), //MaterialApp
   );
 }
